3f81344ac89f69d38bdefb4fa387eaf59ec20596
NelsonLee0124 <a06w.68577818@gmail.com>
2020-12-30 21:29:37 +0800

    Edit data_insight.py

--- data_insight.py
+++ data_insight.py
@@ -7,15 +7,31 @@ import math
 from dataset import Dataset
 
 class DataInsight():
-    def __init__():
+    def __init__(self):
         self.dataset = Dataset()
 
-    def draw_attribute_adr(attribute):
-        attribute = train_df.loc[:, 'total_of_special_requests']
-        adr = 
+    def plot_attribute_adr(self, attribute):
+        attribute = self.dataset.get_column(attribute, True)
+        adr = self.dataset.get_adr(True)
         plt.plot(attribute, adr, ',')
-        plt.ylim(0, 1000)
         plt.show()
 
-    def draw_attribute_is_canceled():
+    def plot_attribute_adr_mean(self, attribute):
+        attrbute = self.dataset.get_column(attribute, True)
+        attribute = list(set(attribute))
+        attribute.sort()
+        avg_list = []
+        num_list = []
+        for i in attribute:
+            avg = np.average(self.dataset.train_df.loc[self.dataset.train_df[attribute] == i].loc[:, 'adr'].to_numpy())
+            num = np.average(len(self.dataset.train_df.loc[train_df['attribute'] == i].loc[:, 'adr'].to_numpy()))
+            avg_list.append(avg)
+            num_list.append(num)
+            print("average of {} = {}, which has {} samples".format(i, avg, num))
+
+        plt.plot(attribute, avg_list, 'r.')
+        # plt.plot(attribute, num_list, 'b.')
+        plt.show()
+
+    def plot_attribute_is_canceled(self):
         pass
---
+++ plot.py
@@ -0,0 +1,4 @@
+from data_insight import DataInsight
+
+data_insight = DataInsight()
+data_insight.plot_attribute_adr_mean('country')
