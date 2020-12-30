7ffe28ac9b6c4278b8ed832ceb06e32cc8417b7a
guan27 <dereklin1027@gmail.com>
2020-12-30 21:18:57 +0800

    update some .py

--- logistic_regression_iscanceled.py
+++ logistic_regression_iscanceled.py
@@ -1,12 +1,13 @@
 import pandas as pd
 import numpy as np
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LogisticRegression
 from dataset import Dataset
 
 hotel = Dataset()
 
 x = hotel.get_dataset(to_numpy = True)
-y = hotel.get_adr(to_numpy = True)
+y = hotel.get_is_canceled(to_numpy = True)
 
-reg = LinearRegression().fit(x, y)
+
+reg = LogisticRegression(max_iter=800).fit(x, y)
 print(reg.score(x, y))
