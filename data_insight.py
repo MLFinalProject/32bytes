5336179765628c5fd89def48cba7a2db6bfc8645
NelsonLee0124 <a06w.68577818@gmail.com>
2020-12-30 20:49:14 +0800

    Add data_insight.py

---
+++ data_insight.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from datetime import date
+import math
+
+from dataset import Dataset
+
+class DataInsight():
+    def __init__():
+        self.dataset = Dataset()
+
+    def draw_attribute_adr(attribute):
+        attribute = train_df.loc[:, 'total_of_special_requests']
+        adr = 
+        plt.plot(attribute, adr, ',')
+        plt.ylim(0, 1000)
+        plt.show()
+
+    def draw_attribute_is_canceled():
+        pass
