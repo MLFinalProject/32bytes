8341be19145cd375d9ff4d5fe4c10f2df648eb05
guan27 <dereklin1027@gmail.com>
2021-01-01 17:03:30 +0800

    add feature_engineering.py big modification on dataset.py

--- dataset.py
+++ dataset.py
@@ -2,7 +2,6 @@ import pandas as pd
 import numpy as np
 from datetime import date
 
-
 class Dataset(object):
     """docstring for Dataset"""
 
@@ -12,29 +11,30 @@ class Dataset(object):
                      'October': 10, 'November': 11, 'December': 12}
 
     def __init__(self):
-        self.train_df = pd.read_csv('./data/train.csv')
-        self.train_df = self.train_df.drop(self.train_df[(self.train_df.adults+self.train_df.babies+self.train_df.children)==0].index)
-        self.train_df['country'].fillna(self.train_df['country'].mode().to_string(), inplace=True)
-        self.train_df['children'].fillna(round(self.train_df['children'].mean()), inplace=True)
-        self.train_df[['agent','company']] = self.train_df[['agent','company']].fillna(0.0)
-        self.train_df[['children', 'company', 'agent']] = self.train_df[['children', 'company', 'agent']].astype('int64')
-        self.target_df = self.train_df.loc[:, ['is_canceled', 'adr']]
-
-        
-        self.train_df = self.train_df.drop(['ID', 'is_canceled', 'adr',
-                                            'reservation_status',
-                                            'reservation_status_date'], axis=1)
-
-        arrival_np = self.train_df.loc[:,['arrival_date_year','arrival_date_month','arrival_date_day_of_month']].to_numpy()
+        train_df = pd.read_csv('./data/train.csv')
+        test_df = pd.read_csv('./data/test.csv')
+        self.target_df = train_df.loc[:, ['is_canceled', 'adr']]
+        train_df.drop(['adr', 'is_canceled','reservation_status','reservation_status_date'], axis = 1, inplace = True)
+        self.train_test_df = pd.concat([train_df.assign(dataset="train"), test_df.assign(dataset="test"),], ignore_index = True)
+        self.train_test_df.drop(['ID'], axis = 1, inplace = True)
+        arrival_np = self.train_test_df.loc[:,['arrival_date_year','arrival_date_month','arrival_date_day_of_month']].to_numpy()
         arrival_np[:,1] = [self.month_str2num[i] for i in arrival_np[:,1]]
-        self.arrival_df = pd.DataFrame([date(i[0],i[1],i[2]).isoformat() for i in arrival_np], columns = ['arrival_date'])
-        self.number_of_days_df = pd.DataFrame(self.train_df['stays_in_weekend_nights'] + 
-                                              self.train_df['stays_in_week_nights'], columns = ['number_of_days'])
-        self.train_df['agent'] = self.train_df['agent'].apply(str)
-        self.train_df['company'] = self.train_df['company'].apply(str)
-        self.train_df['arrival_date_year'] = self.train_df['arrival_date_year'].apply(str)
-        self.train_df['arrival_date_day_of_month'] = self.train_df['arrival_date_day_of_month'].apply(str)
+        self.arrival_date_df = pd.DataFrame([date(i[0],i[1],i[2]).isoformat() for i in arrival_np], columns = ['arrival_date'])
+        self.arrival_date_df['dataset'] = self.train_test_df['dataset']
+        self.number_of_days_df = pd.DataFrame(self.train_test_df['stays_in_weekend_nights'] + 
+                                    self.train_test_df['stays_in_week_nights'], columns = ['number_of_days'])
+        self.number_of_days_df['dataset'] = self.train_test_df['dataset']
 
+        self.train_test_df['country'].fillna(self.train_test_df['country'].mode().to_string(), inplace=True)
+        self.train_test_df['children'].fillna(round(self.train_test_df['children'].mean()), inplace=True)
+        self.train_test_df[['agent','company']] = self.train_test_df[['agent','company']].fillna(0.0)
+        self.train_test_df[['children', 'company', 'agent']] = self.train_test_df[['children', 'company', 'agent']].astype('int64')
+        
+        self.train_test_df['agent'] = self.train_test_df['agent'].apply(str)
+        self.train_test_df['company'] = self.train_test_df['company'].apply(str)
+        self.train_test_df['arrival_date_year'] = self.train_test_df['arrival_date_year'].apply(str)
+        self.train_test_df['arrival_date_day_of_month'] = self.train_test_df['arrival_date_day_of_month'].apply(str)
+        
     def smooth_target_encoding(self, column_name, target, weight = 300, replace = True):
         global_mean = self.target_df[target].mean()
         temp_df = pd.concat([self.train_df[column_name], self.target_df[target]], axis=1)
@@ -43,46 +43,62 @@ class Dataset(object):
         categorial_mean = agg['mean']
         smooth = (categorial_count * categorial_mean + weight * global_mean) / (categorial_count + weight)
         if replace:
-            self.train_df[column_name] = temp_df[column_name].map(smooth)
+            self.train_test_df[column_name] = temp_df[column_name].map(smooth)
         else:
-            self.train_df[column_name + '_target_encoding'] = temp_df[column_name].map(smooth)
+            self.train_test_df[column_name + '_target_encoding'] = temp_df[column_name].map(smooth)
         return
-    def one_hot_encoding(self, column_name):
-        self.train_df[column_name] = pd.get_dummies(self.train_df[column_name], prefix = column_name)
-        return
-    def get_arrival_date(self, to_numpy = False):
-        if to_numpy:
-            return self.arrival_df.to_numpy().squeeze()
-        else:
-            return self.arrival_df
-    def get_number_of_days(self, to_numpy = False):
-        if to_numpy:
-            return self.number_of_days_df.to_numpy().squeeze()
-        else:
-            return self.number_of_days_df
-    def get_adr(self, to_numpy = False):
-        if to_numpy:
-            return self.target_df[['adr']].to_numpy().squeeze()
-        else:
-            return self.target_df[['adr']]
-    def get_is_canceled(self, to_numpy = False):
-        if to_numpy:
-            return self.target_df[['is_canceled']].to_numpy().squeeze()
-        else:
-            return self.target_df[['is_canceled']]
-    def get_column(self, column_name, to_numpy = False):
-        if to_numpy:
-            return self.train_df[[column_name]].to_numpy().squeeze()
-        else:
-            return self.train_df[[column_name]]
-    def get_train_dataset(self, to_numpy = False):
-        if to_numpy:
-            return pd.get_dummies(self.train_df).to_numpy()
-        else:
-            return pd.get_dummies(self.train_df)
-    def get_test_dataset(self, to_numpy = False):
+    def one_hot_encoding(self, column_name, replace = True):
+        self.train_test_df = pd.concat([self.train_test_df, pd.get_dummies(self.train_test_df[column_name], prefix = column_name)],axis = 1)
+        if replace:
+            self.train_test_df.drop([column_name], axis = 1, inplace = True)
         return
 
+
+    def add_feature(self, df):
+        self.train_test_df = pd.concat([self.train_test_df, df], axis=1)
+        
+    def get_feature(self, column_name_list):
+        return self.train_test_df[column_name_list]
+
+    def remove_feature(self, column_name_list):
+        self.train_test_df.drop(column_name_list, axis = 1, inplace = True)
+
+
+
+    def get_train_dataset(self):
+        train_df = self.train_test_df[self.train_test_df['dataset'].eq("train")].drop(['dataset'], axis = 1)
+        return pd.get_dummies(train_df)
+
+    def get_train_arrival_date(self):
+        temp_df = self.arrival_date_df[self.arrival_date_df['dataset'].eq("train")].drop(['dataset'], axis = 1)
+        return temp_df
+
+    def get_train_number_of_days(self):
+        temp_df = self.number_of_days_df[self.number_of_days_df['dataset'].eq("train")].drop(['dataset'], axis = 1)
+        return temp_df
+
+    def get_train_column(self, column_name):
+        temp_df = self.train_test_df[self.train_test_df['dataset'].eq("train")]
+        return temp_df[[column_name]]
+
+    def get_train_adr(self):
+        return self.target_df[['adr']]
+
+    def get_train_is_canceled(self):
+        return self.target_df[['is_canceled']]
+    
+    
+    def get_test_dataset(self):
+        test_df = self.train_test_df[self.train_test_df['dataset'].eq("test")].drop(['dataset'], axis = 1)
+        return pd.get_dummies(test_df)
+
+    def get_test_arrival_date(self):
+        temp_df = self.arrival_date_df[self.arrival_date_df['dataset'].eq("test")].drop(['dataset'], axis = 1)
+        return temp_df
+
+    def get_test_number_of_days(self):
+        temp_df = self.number_of_days_df[self.number_of_days_df['dataset'].eq("test")].drop(['dataset'], axis = 1)
+        return temp_df
 
 
 
---
+++ feature_engineering.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+
+def gen_room_feature(data_frame):
+	data_frame = data_frame.assign(room=0)
+	data_frame.loc[ data_frame['reserved_room_type'] == data_frame['assigned_room_type'] , 'room'] = 1
+	return data_frame[['room']]
+
+def gen_net_canceled_feature(data_frame):
+	data_frame = data_frame.assign(net_canceled=0)
+	data_frame.loc[ data_frame['previous_cancellations'] > data_frame['previous_bookings_not_canceled'] , 'net_canceled'] = 1
+	return data_frame[['net_canceled']]
+
