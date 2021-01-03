3a59ed85c5d68df8562fd32aa85df4df6dd0120b
NelsonLee0124 <a06w.68577818@gmail.com>
2021-01-04 03:19:00 +0800

    Plot all adr and is_canceled relationship

--- data_insight.py
+++ data_insight.py
@@ -10,31 +10,48 @@ class DataInsight():
     def __init__(self):
         self.dataset = Dataset()
         self.train_df = self.dataset.train_test_df[self.dataset.train_test_df['dataset'].eq(self.dataset.data_label["train"])].drop(['dataset'], axis=1)
+        self.train_df['adr'] = self.dataset.target_df['adr']
+        self.train_df['is_canceled'] = self.dataset.target_df['is_canceled']
 
     def plot_attribute_adr(self, attribute):
-        attribute = self.dataset.get_train_column(attribute)
-        print(attribute)
-        adr = self.dataset.get_train_adr()
-        print(adr)
-        plt.plot(attribute, adr, ',')
-        plt.show()
+        attribute_element = self.dataset.get_train_column(attribute).to_numpy().squeeze()
+        # print(attribute)
+        adr = self.dataset.get_train_adr().to_numpy().squeeze()
+        # print(adr)
+        plt.figure()
+        plt.plot(attribute_element, adr, ',')
+        plt.savefig("./data_adr_analysis/{}_adr.png".format(attribute))
+        # plt.show()
 
     def plot_attribute_adr_mean(self, attribute):
-        attrbute = self.dataset.get_train_column(attribute)
-        attribute = list(set(attribute))
-        attribute.sort()
+        attribute_element = self.dataset.get_train_column(attribute).to_numpy().squeeze()
+        attribute_element = list(set(attribute_element))
+        attribute_element.sort()
         avg_list = []
         num_list = []
-        for i in attribute:
+        for i in attribute_element:
             avg = np.average(self.train_df.loc[self.train_df[attribute] == i].loc[:, 'adr'].to_numpy())
-            num = np.average(len(self.train_df.loc[self.train_df['attribute'] == i].loc[:, 'adr'].to_numpy()))
+            num = np.average(len(self.train_df.loc[self.train_df[attribute] == i].loc[:, 'adr'].to_numpy()))
             avg_list.append(avg)
             num_list.append(num)
-            print("average of {} = {}, which has {} samples".format(i, avg, num))
+            # print("average of {} = {}, which has {} samples".format(i, avg, num))
+        plt.figure()
+        plt.plot(attribute_element, avg_list, 'r.')
+        plt.savefig("./data_adr_analysis/{}_adr_mean.png".format(attribute))
+        # plt.show()
 
-        plt.plot(attribute, avg_list, 'r.')
-        # plt.plot(attribute, num_list, 'b.')
-        plt.show()
-
-    def plot_attribute_is_canceled(self):
-        pass
+    def plot_attribute_is_canceled_mean(self, attribute):
+        attribute_element = self.dataset.get_train_column(attribute).to_numpy().squeeze()
+        attribute_element = list(set(attribute_element))
+        attribute_element.sort()
+        avg_list = []
+        num_list = []
+        for i in attribute_element:
+            avg = np.average(self.train_df.loc[self.train_df[attribute] == i].loc[:, 'is_canceled'].to_numpy())
+            num = np.average(len(self.train_df.loc[self.train_df[attribute] == i].loc[:, 'is_canceled'].to_numpy()))
+            avg_list.append(avg)
+            num_list.append(num)
+            # print("average of {} = {}, which has {} samples".format(i, avg, num))
+        plt.figure()
+        plt.plot(attribute_element, avg_list, 'r.')
+        plt.savefig("./data_is_canceled_analysis/{}_is_canceled_mean.png".format(attribute))
--- plot.py
+++ plot.py
@@ -7,4 +7,9 @@ data_insight = DataInsight()
 # train_test_df = pd.get_dummies(dataset.train_test_df)
 # print(dataset.train_test_df)
 # print(dataset.get_train_dataset())
-data_insight.plot_attribute_adr('country')
+
+attribute_list = list(data_insight.train_df.columns.values)[:-2]
+for attribute in attribute_list:
+    # data_insight.plot_attribute_adr(attribute)
+    # data_insight.plot_attribute_adr_mean(attribute)
+    data_insight.plot_attribute_is_canceled_mean(attribute)
