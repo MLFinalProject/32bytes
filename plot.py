9735bf11fe4cb8db9321dd80fd55457b8c721a76
NelsonLee0124 <a06w.68577818@gmail.com>
2021-01-04 17:03:11 +0800

    Fix plotting

--- plot.py
+++ plot.py
@@ -8,10 +8,12 @@ data_insight = DataInsight()
 # print(dataset.train_test_df)
 # print(dataset.get_train_dataset())
 
-attribute_list = list(data_insight.train_df.columns.values)[:-2]
-for attribute in attribute_list:
+print(data_insight.train_df.country)
+
+# attribute_list = list(data_insight.train_df.columns.values)[:-2]
+# for attribute in attribute_list:
     # data_insight.plot_attribute_adr(attribute)
-    data_insight.plot_attribute_adr_mean(attribute)
+    # data_insight.plot_attribute_adr_mean(attribute)
     # data_insight.plot_attribute_is_canceled_mean(attribute)
 
 
