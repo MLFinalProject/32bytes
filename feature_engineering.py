85f50e34c5f48a10615a6c99b16541ad00b5d9ca
solorush <b05502063@ntu.edu.tw>
2021-01-06 20:46:39 +0800

    add dataset.py

--- feature_engineering.py
+++ feature_engineering.py
@@ -11,3 +11,21 @@ def gen_net_canceled_feature(data_frame):
 	data_frame.loc[ data_frame['previous_cancellations'] > data_frame['previous_bookings_not_canceled'] , 'net_canceled'] = 1
 	return data_frame[['net_canceled']]
 
+def transfer_not_enough_data_to_mean(data_frame, threshold):
+	attribute_mean = round(data_frame.mean())
+	column_name = data_frame.columns[0]
+	data_frame[column_name].values[data_frame[column_name] > threshold] = attribute_mean + 100
+	return data_frame
+
+def absolute_peak_transform(data_frame, peak):
+	column_name = data_frame.columns[0]
+	# attribute = data_frame[column_name]
+	# print(data_frame[column_name])
+	df = abs(data_frame.loc[:, column_name] - peak)
+	# print(df)
+	return df
+	# exit()
+	# # data_frame.loc[:, [column_name]] += 1
+	# print(data_frame[column_name])
+	# exit()
+	# return data_frame
