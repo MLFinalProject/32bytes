0330b9ec67c9e963391889cbf95c2a220ed3080f
guan27 <dereklin1027@gmail.com>
2021-01-08 22:19:56 +0800

    err count

--- regression.py
+++ regression.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error
 from sklearn.linear_model import LinearRegression
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import GradientBoostingRegressor
@@ -24,7 +25,7 @@ class Regression:
         self.month_str = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December',]
         self.start_time = time.time()
         self.x_val_train, self.x_val_test, self.y_val_train, self.y_val_test = train_test_split(
-            self.x_train, self.y_train, test_size=0.2, random_state=seed)
+            self.x_train, self.y_train, test_size=0.5, random_state=seed)
       
         self.y_val_test = pd.DataFrame(self.y_val_test.tolist(),columns=['y'])
         self.x_val_test = pd.concat([self.x_val_test.reset_index(drop=True), self.y_val_test],axis=1)
@@ -56,24 +57,27 @@ class TheRandomForestRegressor(Regression):
     def v_fold_validate(self):
         super().v_fold_validate()
         self.reg = self.reg.fit(self.x_val_train, self.y_val_train)
-        train_acc = self.reg.score(self.x_val_train, self.y_val_train)
-        test_acc = self.reg.score(self.x_val_test, self.y_val_test)
-        print(f'Training Accuracy of our model is: {train_acc:.3f}')
-        print(f'Test Accuracy of our model is: {test_acc:.3f}')
+        train_err = self.reg.score(self.x_val_train, self.y_val_train)
+        test_err = self.reg.score(self.x_val_test, self.y_val_test)
+        print(f'Training Accuracy of our model is: {train_err:.3f}')
+        print(f'Test Accuracy of our model is: {test_err:.3f}')
         print(f'adr validation done in {time.time()-self.start_time:.3f}(s).')
 
     def monthly_validate(self, seed = None):
         super().monthly_validate(seed)
         self.reg = self.reg.fit(self.x_val_train, self.y_val_train)
-        train_acc = self.reg.score(self.x_val_train, self.y_val_train)
-        test_acc = self.reg.score(self.x_val_test, self.y_val_test)
-        print(f'Overall||\ntrain_acc: {train_acc:.3f}\ntest_acc: {test_acc:.3f}')
+        y_train_pred = self.reg.predict(self.x_val_train)
+        y_test_pred = self.reg.predict(self.x_val_test)
+        train_err = mean_absolute_error(y_train_pred, self.y_val_train)
+        test_err = mean_absolute_error(y_test_pred, self.y_val_test)
+        print(f'Overall||\ntrain_err: {train_err:.3f}\ntest_err: {test_err:.3f}')
         print('--------------------\nMonthly||')
         month_acc = []
         for m in self.month_str:
-            test_acc = self.reg.score(self.x_month_test[m], self.y_month_test[m])
-            month_acc.append(test_acc)
-            print(f'test_acc: {test_acc:.3f} ({m})')
+            y_test_pred = self.reg.predict(self.x_month_test[m])
+            test_err = mean_absolute_error(y_test_pred, self.y_month_test[m])
+            month_acc.append(test_err)
+            print(f'test_err: {test_err:.3f} ({m})')
         print(f'mean: {np.mean(month_acc):.3f}, std: {np.std(month_acc):.3f}, max: {np.max(month_acc):.3f}, min: {np.min(month_acc):.3f}')
         print(f'mean: {np.mean(month_acc[3:8]):.3f}, std: {np.std(month_acc[3:8]):.3f}, max: {np.max(month_acc[3:8]):.3f}, min: {np.min(month_acc[3:8]):.3f} (April-August)')
         print(f'done in {time.time()-self.start_time:.3f}(s).\n')
@@ -84,11 +88,11 @@ class TheRandomForestRegressor(Regression):
 
     def ensemble_seed(self, seed):
         self.start_time = time.time()
-        self.reg = RandomForestRegressor(min_impurity_decrease=0.001, max_features=0.4, min_samples_leaf = 2, n_estimators=128, random_state = seed, bootstrap=True, n_jobs = -1)
+        self.reg = RandomForestRegressor(min_impurity_decrease=0.001, max_features=0.4, min_samples_leaf = 2, n_estimators=128, random_state = seed, n_jobs = -1)
         self.reg = self.reg.fit(self.x_train,self.y_train)
-        train_acc = self.reg.score(self.x_train,self.y_train)
+        train_err = self.reg.score(self.x_train,self.y_train)
         predicts = pd.DataFrame(self.reg.predict(self.x_test), columns = ['adr'])
-        print(f'Regression Accuracy: {train_acc:.3f}', end = '\t')
+        print(f'Regression Accuracy: {train_err:.3f}', end = '\t')
         print(f'done in {time.time()-self.start_time:.3f}(s).')
         return predicts
     
@@ -99,17 +103,17 @@ class TheRandomForestRegressor(Regression):
             self.x_val_train, self.x_val_test, self.y_val_train, self.y_val_test = train_test_split(
                 self.x_train, self.y_train, test_size=0.2, random_state=seed)
             self.reg = self.reg.fit(self.x_val_train, self.y_val_train)
-            train_acc = self.reg.score(self.x_val_train, self.y_val_train)
-            test_acc = self.reg.score(self.x_val_test, self.y_val_test)
-            print(f'seed {seed}\t train_acc:{train_acc:.3f}, test_acc:{test_acc:.3f}')
+            train_err = self.reg.score(self.x_val_train, self.y_val_train)
+            test_err = self.reg.score(self.x_val_test, self.y_val_test)
+            print(f'seed {seed}\t train_err:{train_err:.3f}, test_err:{test_err:.3f}')
         print(f'experiment done in {time.time()-self.start_time:.3f}(s).')
         print('--------------------\n')
 
     def train(self):
         super().train()
         self.reg = self.reg.fit(self.x_train,self.y_train)
-        train_acc = self.reg.score(self.x_train,self.y_train)
-        print(f'Training Accuracy of our model is: {train_acc:.3f}')
+        train_err = self.reg.score(self.x_train,self.y_train)
+        print(f'Training Accuracy of our model is: {train_err:.3f}')
         print(f'adr training done in {time.time()-self.start_time:.3f}(s).')
         
     def predict(self):
@@ -121,32 +125,51 @@ class TheRandomForestRegressor(Regression):
 
 		
 # class TheLinearRegression(Regression):
-# 	def __init__(self, x_train, y_val_train, x_test):
-# 		super().__init__(x_train, y_train, x_test)
-# 		self.reg = LinearRegression()
+#     def __init__(self, x_train, y_train, x_test):
+#         super().__init__(x_train, y_train, x_test)
+#         self.reg = LinearRegression()
+
+#     def v_fold_validate(self):
+#         super().v_fold_validate()
+#         self.reg = self.reg.fit(self.x_val_train, self.y_val_train)
+#         train_err = self.reg.score(self.x_val_train, self.y_val_train)
+#         test_err = self.reg.score(self.x_val_test, self.y_val_test)
+#         print('---Cross-Validation Testing---')
+#         print(f'Training Accuracy of our model is: {train_err}')
+#         print(f'Cross-Validation Test Accuracy of our model is: {test_err}')
+#         print(f'adr validation done in {time.time()-self.start_time:.3f}(s).')
 	
-# 	def v_fold_validate(self):
-# 		super().v_fold_validate()
-# 		self.reg = self.reg.fit(self.x_val_train, self.y_val_train)
-# 		train_acc = self.reg.score(self.x_val_train, self.y_val_train)
-# 		test_acc = self.reg.score(self.x_val_test, self.y_val_test)
-# 		print('---Cross-Validation Testing---')
-# 		print(f'Training Accuracy of our model is: {train_acc}')
-# 		print(f'Cross-Validation Test Accuracy of our model is: {test_acc}')
-# 		print(f'adr validation done in {time.time()-self.start_time:.3f}(s).')
-	
-# 	def train(self):
-# 		super().train()
-# 		self.reg = self.reg.fit(self.x_train,self.y_train)
-# 		train_acc = self.reg.score(self.x_train,self.y_train)
-# 		print(f'Training Accuracy of our model is: {train_acc:.3f}')
-# 		print(f'adr training done in {time.time()-self.start_time:.3f}(s).')
-		
-# 	def predict(self):
-# 		super().predict()
-# 		predicts = pd.DataFrame(self.reg.predict(self.x_test), columns = ['adr'])
-# 		print(f'adr prediction done in {time.time()-self.start_time:.3f}(s).')
-# 		return predicts
+#     def train(self):
+#         super().train()
+#         self.reg = self.reg.fit(self.x_train,self.y_train)
+#         train_err = self.reg.score(self.x_train,self.y_train)
+#         print(f'Training Accuracy of our model is: {train_err:.3f}')
+#         print(f'adr training done in {time.time()-self.start_time:.3f}(s).')
+
+#     def monthly_validate(self, seed = None):
+#         super().monthly_validate(seed)
+#         self.reg = self.reg.fit(self.x_val_train, self.y_val_train)
+#         y_train_pred = self.reg.predict(self.x_val_train)
+#         y_test_pred = self.reg.predict(self.x_val_test)
+#         train_err = mean_absolute_error(y_train_pred, self.y_val_train)
+#         test_err = mean_absolute_error(y_test_pred, self.y_val_test)
+#         print(f'Overall||\ntrain_err: {train_err:.3f}\ntest_err: {test_err:.3f}')
+#         print('--------------------\nMonthly||')
+#         month_acc = []
+#         for m in self.month_str:
+#             y_test_pred = self.reg.predict(self.x_month_test[m])
+#             test_err = mean_absolute_error(y_test_pred, self.y_month_test[m])
+#             month_acc.append(test_err)
+#             print(f'test_err: {test_err:.3f} ({m})')
+    #     print(f'mean: {np.mean(month_acc):.3f}, std: {np.std(month_acc):.3f}, max: {np.max(month_acc):.3f}, min: {np.min(month_acc):.3f}')
+    #     print(f'mean: {np.mean(month_acc[3:8]):.3f}, std: {np.std(month_acc[3:8]):.3f}, max: {np.max(month_acc[3:8]):.3f}, min: {np.min(month_acc[3:8]):.3f} (April-August)')
+    #     print(f'done in {time.time()-self.start_time:.3f}(s).\n')
+    
+    # def predict(self):
+    #     super().predict()
+    #     predicts = pd.DataFrame(self.reg.predict(self.x_test), columns = ['adr'])
+    #     print(f'adr prediction done in {time.time()-self.start_time:.3f}(s).')
+    #     return predicts
 
 # class TheDecisionTreeRegressor(Regression):
 # 	def __init__(self, x_train, y_train, x_test):
@@ -156,18 +179,18 @@ class TheRandomForestRegressor(Regression):
 # 	def v_fold_validate(self):
 # 		super().v_fold_validate()
 # 		self.reg = self.reg.fit(self.x_val_train, self.y_val_train)
-# 		train_acc = self.reg.score(self.x_val_train, self.y_val_train)
-# 		test_acc = self.reg.score(self.x_val_test, self.y_val_test)
+# 		train_err = self.reg.score(self.x_val_train, self.y_val_train)
+# 		test_err = self.reg.score(self.x_val_test, self.y_val_test)
 # 		print('---Cross-Validation Testing---')
-# 		print(f'Training Accuracy of our model is: {train_acc:.3f}')
-# 		print(f'Cross-Validation Test Accuracy of our model is: {test_acc:.3f}')
+# 		print(f'Training Accuracy of our model is: {train_err:.3f}')
+# 		print(f'Cross-Validation Test Accuracy of our model is: {test_err:.3f}')
 # 		print(f'adr validation done in {time.time()-self.start_time:.3f}(s).')
 	
 # 	def train(self):
 # 		super().train()
 # 		self.reg = self.reg.fit(self.x_train,self.y_train)
-# 		train_acc = self.reg.score(self.x_train,self.y_train)
-# 		print(f'Training Accuracy of our model is: {train_acc:.3f}')
+# 		train_err = self.reg.score(self.x_train,self.y_train)
+# 		print(f'Training Accuracy of our model is: {train_err:.3f}')
 # 		print(f'adr training done in {time.time()-self.start_time:.3f}(s).')
 		
 # 	def predict(self):
@@ -184,18 +207,18 @@ class TheRandomForestRegressor(Regression):
 # 	def v_fold_validate(self):
 # 		super().v_fold_validate()
 # 		self.reg = self.reg.fit(self.x_val_train, self.y_val_train)
-# 		train_acc = self.reg.score(self.x_val_train, self.y_val_train)
-# 		test_acc = self.reg.score(self.x_val_test, self.y_val_test)
+# 		train_err = self.reg.score(self.x_val_train, self.y_val_train)
+# 		test_err = self.reg.score(self.x_val_test, self.y_val_test)
 # 		print('---Cross-Validation Testing---')
-# 		print(f'Training Accuracy of our model is: {train_acc:.3f}')
-# 		print(f'Cross-Validation Test Accuracy of our model is: {test_acc:.3f}')
+# 		print(f'Training Accuracy of our model is: {train_err:.3f}')
+# 		print(f'Cross-Validation Test Accuracy of our model is: {test_err:.3f}')
 # 		print(f'adr validation done in {time.time()-self.start_time:.3f}(s).')
 	
 # 	def train(self):
 # 		super().train()
 # 		self.reg = self.reg.fit(self.x_train,self.y_train)
-# 		train_acc = self.reg.score(self.x_train,self.y_train)
-# 		print(f'Training Accuracy of our model is: {train_acc:.3f}')
+# 		train_err = self.reg.score(self.x_train,self.y_train)
+# 		print(f'Training Accuracy of our model is: {train_err:.3f}')
 # 		print(f'adr training done in {time.time()-self.start_time:.3f}(s).')
 		
 # 	def predict(self):
@@ -220,16 +243,19 @@ if __name__ == '__main__':
     # new_attribute_df = absolute_peak_transform(hotel_adr.get_feature([modified_key]), peak)
     # hotel_adr.remove_feature([modified_key])
     # hotel_adr.add_feature(new_attribute_df)
-    hotel_adr.train_test_df['arrival_date_week_number'] = hotel_adr.train_test_df['arrival_date_week_number'].apply(str)
-    # hotel_adr.remove_feature(['company'])
+    # hotel_adr.train_test_df['arrival_date_week_number'] = hotel_adr.train_test_df['arrival_date_week_number'].apply(str)
+    # hotel_adr.remove_feature(['agent','company'])
+    room_feature = gen_room_feature(hotel_adr.get_feature(['reserved_room_type', 'assigned_room_type']))
+    net_canceled_feature = gen_net_canceled_feature(hotel_adr.get_feature(['previous_cancellations', 'previous_bookings_not_canceled']))
+    hotel_adr.add_feature(room_feature)
+    hotel_adr.add_feature(net_canceled_feature)
     x_train_adr = hotel_adr.get_train_dataset()
     x_test_adr = hotel_adr.get_test_dataset()
     y_train_adr = hotel_adr.get_train_adr()
     reg = TheRandomForestRegressor(x_train_adr, y_train_adr, x_test_adr)
-    for i in [.2, .25, .3]:
-        print(f'doing max_features = {i}:')
-        reg.reg = RandomForestRegressor(max_features=i, min_samples_leaf = 1, n_estimators=512, max_depth = None, random_state = 1126, bootstrap=True, n_jobs = -1)            
-        reg.monthly_validate(1126)
+    reg.reg = RandomForestRegressor(min_impurity_decrease=0.001, max_features=.55, min_samples_leaf = 2, n_estimators=128, random_state = 123, bootstrap=True, n_jobs = -1)
+    reg.monthly_validate(1126)
+
     # reg.three_seed_validate()
     # exit()
 
