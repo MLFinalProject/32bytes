8e0399e56e96b8f5cb4cee1504bc7c32fb3137e6
b05502063 <b05502063@ntu.edu.tw>
2021-01-08 23:36:01 +0800

    modify

--- main.py
+++ main.py
@@ -8,8 +8,9 @@ from predict import predict
 from encoder import *
 
 
-encode_target = ['hotel','agent','arrival_date_day_of_month','arrival_date_year','assigned_room_type','company','country','customer_type','deposit_type','distribution_channel','market_segment','meal','reserved_room_type']
-#encode_target = ['country']
+#encode_target = ['hotel','agent','arrival_date_day_of_month','assigned_room_type','company','country','customer_type','deposit_type','distribution_channel','market_segment','meal','reserved_room_type']
+target_encode_item = ['deposit_type','customer_type']
+count_encode_item = ['agent','country','market_segment']
 
 hotel_is_cancel = Dataset()
 
@@ -31,41 +32,101 @@ room_feature = gen_room_feature(hotel_is_cancel.get_feature(['reserved_room_type
 net_canceled_feature = gen_net_canceled_feature(hotel_is_cancel.get_feature(['previous_cancellations', 'previous_bookings_not_canceled']))
 hotel_is_cancel.add_feature(room_feature)
 hotel_is_cancel.add_feature(net_canceled_feature)
-#hotel_is_cancel.remove_feature(['country','arrival_date_year'])
 
-data_not_encoded = pd.concat([hotel_is_cancel.get_feature(encode_target),hotel_is_cancel.get_train_is_canceled()],axis = 1)
-data_encoded = count_encode(data_not_encoded[encode_target],data_not_encoded['is_canceled'])
-data_encoded_col_name = data_encoded.columns
-for col in data_encoded_col_name:
+# ---remove only_feature---
+# country_column = hotel_is_cancel.get_feature(['country'])
+# remove_only_list = ['country', 'agent', 'company']
+# for only_attribute in remove_only_list:
+    # attribute_train_column = hotel_is_cancel.get_train_column(only_attribute)
+    # attribute_test_column = hotel_is_cancel.get_test_column(only_attribute)
+    # new_attribute_column = remove_only(hotel_is_cancel.get_feature([only_attribute]), attribute_train_column, attribute_test_column)
+    # # print(new_country_column)
+    # # print(new_attribute_column[only_attribute].value_counts().loc['RMV'])
+    # hotel_is_cancel.remove_feature([only_attribute])
+    # hotel_is_cancel.add_feature(new_attribute_column)
+# exit()
+# -------------------------
+
+# From Hsu
+data_not_encoded = pd.concat([hotel_is_cancel.get_feature(target_encode_item),hotel_is_cancel.get_train_is_canceled()],axis = 1)
+data_encoded = target_encode(data_not_encoded[target_encode_item],data_not_encoded['is_canceled'])
+for col in data_encoded.columns:
 	data_encoded[col].fillna(data_encoded[col].mean(),inplace = True)
 hotel_is_cancel.add_feature(data_encoded)
-hotel_is_cancel.remove_feature(encode_target)
+hotel_is_cancel.remove_feature(target_encode_item)
+
+data_not_encoded = pd.concat([hotel_is_cancel.get_feature(count_encode_item),hotel_is_cancel.get_train_is_canceled()],axis = 1)
+data_encoded = target_encode(data_not_encoded[count_encode_item],data_not_encoded['is_canceled'])
+for col in data_encoded.columns:
+	data_encoded[col].fillna(data_encoded[col].mean(),inplace = True)
+hotel_is_cancel.add_feature(data_encoded)
+hotel_is_cancel.remove_feature(count_encode_item)
+
+# ---Remove only---
+
+hotel_is_cancel.remove_feature(['arrival_date_year'])
 
 x_train_is_canceled = hotel_is_cancel.get_train_dataset()
 x_test_is_canceled = hotel_is_cancel.get_test_dataset()
 y_train_is_canceled = hotel_is_cancel.get_train_is_canceled()
+
+# print(type(hotel_is_cancel))
+# print(type(x_train_is_canceled))
+# for only_attribute in remove_only_list:
+    # remove_string = '{}_RMV'.format(only_attribute)
+    # x_train_is_canceled.drop([remove_string], axis=1, inplace=True)
+    # x_test_is_canceled.drop([remove_string], axis=1, inplace=True)
+
+# print(x_train_is_canceled)sx
+# print(x_test_is_canceled)
+# print(y_train_is)
+
+# ------
+
 clf = TheRandomForest(x_train_is_canceled, y_train_is_canceled, x_test_is_canceled)
-clf.v_fold_validate()
+# clf.v_fold_validate()
 
 clf.train()
 is_canceled_df = clf.predict()
 
 
 hotel_adr = Dataset()
-#hotel_adr.remove_feature(['country','arrival_date_year'])
 
-data_not_encoded = pd.concat([hotel_adr.get_feature(encode_target),hotel_adr.get_train_adr()],axis = 1)
-data_encoded = count_encode(data_not_encoded[encode_target],data_not_encoded['adr'])
-for col in data_encoded_col_name:
-	data_encoded[col].fillna(data_encoded[col].mean(),inplace = True)
-hotel_adr.add_feature(data_encoded)
-hotel_adr.remove_feature(encode_target)
+# From Hsu
+# data_not_encoded = pd.concat([hotel_adr.get_feature(encode_target),hotel_adr.get_train_adr()],axis = 1)
+# data_encoded = target_encode(data_not_encoded[encode_target],data_not_encoded['adr'])
+# for col in data_encoded_col_name:
+	# data_encoded[col].fillna(data_encoded[col].mean(),inplace = True)
+# hotel_adr.add_feature(data_encoded)
+# hotel_adr.remove_feature(encode_target)
+
+# ---remove only_feature---
+# country_column = hotel_adr.get_feature(['country'])
+# remove_only_list = ['country', 'agent', 'company']
+# for only_attribute in remove_only_list:
+    # attribute_train_column = hotel_adr.get_train_column(only_attribute)
+    # attribute_test_column = hotel_adr.get_test_column(only_attribute)
+    # new_attribute_column = remove_only(hotel_adr.get_feature([only_attribute]), attribute_train_column, attribute_test_column)
+    # # print(new_country_column)
+    # # print(new_attribute_column[only_attribute].value_counts().loc['RMV'])
+    # hotel_adr.remove_feature([only_attribute])
+    # hotel_adr.add_feature(new_attribute_column)
+# exit()
+# -------------------------
+hotel_adr.remove_feature(['arrival_date_year'])
 
 x_train_adr = hotel_adr.get_train_dataset()
 x_test_adr = hotel_adr.get_test_dataset()
 y_train_adr = hotel_adr.get_train_adr()
+
+# for only_attribute in remove_only_list:
+    # remove_string = '{}_RMV'.format(only_attribute)
+    # x_train_adr.drop([remove_string], axis=1, inplace=True)
+    # x_test_adr.drop([remove_string], axis=1, inplace=True)
+
+
 reg = TheRandomForestRegressor(x_train_adr, y_train_adr, x_test_adr)
-reg.v_fold_validate()
+# reg.v_fold_validate()
 
 reg.train()
 adr_df = reg.predict()
